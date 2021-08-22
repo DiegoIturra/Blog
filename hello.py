28948d46593a30f8dcf6ab77af1771e61b431fe4
Diego Iturra <diturra@dparadig.com>
2021-08-22 00:45:00 -0400

    ADD: WTF form added

--- hello.py
+++ hello.py
@@ -1,13 +1,33 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for,session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField , SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "HARD TO GUEESS STRING"
+
 bootstrap = Bootstrap(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+	name = StringField("What is your name?" , validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
+
+
+
+@app.route('/' , methods=["GET","POST"])
 def index():
-	return render_template('index.html')
+	name = None
+	form = NameForm()
+
+	#if form is submitted
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ""
+	return render_template('index.html',form=form,name=name)
 
 
 @app.route('/user/<name>')
