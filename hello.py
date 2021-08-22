41e453c911c31f6b1a18f8a020ef7eab55218c3f
Diego Iturra <diturra@dparadig.com>
2021-08-21 22:21:38 -0400

    ADD: basic setup project

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html',name=name)
+
+app.run(debug=True)
