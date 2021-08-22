9075da54c24c0bba7ea9d7e9d2f095b2ef993ed8
Diego Iturra <diturra@dparadig.com>
2021-08-22 16:43:49 -0400

    ADD: flash messages

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,redirect,url_for,session
+from flask import Flask,render_template,redirect,url_for,session,flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField , SubmitField
@@ -16,18 +16,18 @@ class NameForm(FlaskForm):
 
 
 
-
-
 @app.route('/' , methods=["GET","POST"])
 def index():
-	name = None
 	form = NameForm()
 
 	#if form is submitted
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ""
-	return render_template('index.html',form=form,name=name)
+		old_name = session.get("name")
+		if old_name is not None and old_name != form.name.data:
+			flash("Looks like you have changed your name!")
+		session["name"] = form.name.data
+		return redirect(url_for("index"))
+	return render_template('index.html',form=form,name=session.get("name"))
 
 
 @app.route('/user/<name>')
