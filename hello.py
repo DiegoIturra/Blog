b3c9acfd0aa86655660ce958b94125fa9ae64261
Diego Iturra <diturra@dparadig.com>
2021-08-24 22:09:27 -0400

    ADD: migrations Scripts

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,redirect,url_for,session,flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from wtforms import StringField , SubmitField
 from wtforms.validators import DataRequired
 import os
@@ -15,7 +16,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
-
+migrate = Migrate(app,db)
 
 class NameForm(FlaskForm):
 	name = StringField("What is your name?" , validators=[DataRequired()])
@@ -51,12 +52,24 @@ def index():
 
 	#if form is submitted
 	if form.validate_on_submit():
-		old_name = session.get("name")
-		if old_name is not None and old_name != form.name.data:
-			flash("Looks like you have changed your name!")
+
+		#query in database filter by username
+		user = User.query.filter_by(username=form.name.data).first()
+
+		#if the username doesn't exist
+		#then add to the database
+		if user is None:
+			user = User(username=form.name.data)
+			db.session.add(user)
+			db.session.commit()
+			session["known"] = False
+		#else session["known"] is True
+		else:
+			session["known"] = True
 		session["name"] = form.name.data
+		form.name.data = ""
 		return redirect(url_for("index"))
-	return render_template('index.html',form=form,name=session.get("name"))
+	return render_template('index.html',form=form,name=session.get("name"),known=session.get("known"))
 
 
 @app.route('/user/<name>')
@@ -73,5 +86,10 @@ def page_not_found(e):
 def internal_server_error(e):
 	return render_template('500.html'),500
 
+@app.shell_context_processor
+def make_shell_context():
+	return dict(db=db,User=User,Role=Role)
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+	app.run(debug=True)
