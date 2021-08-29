1c5d797c00b990cda874f9c2ca6a12fc01eb46e6
Diego Iturra <diturra@dparadig.com>
2021-08-29 15:45:49 -0400

    FIX: Delete personal information

--- hello.py
+++ hello.py
@@ -23,8 +23,8 @@ app.config["FLASKY_MAIL_SENDER"] = "Flasky Admin <diego269707@gmail.com>"
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "diego269707@gmail.com"
-app.config["MAIL_PASSWORD"] = "FLASKY1997diego"
+app.config["MAIL_USERNAME"] = "your email"
+app.config["MAIL_PASSWORD"] = "yor password"
 
 mail = Mail(app)
 bootstrap = Bootstrap(app)
